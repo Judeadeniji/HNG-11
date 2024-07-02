291ba4812990131d886ca10778905975f8a2d038
Judeadeniji <judeadeniji66@gmail.com>
2024-07-02 23:52:38 +0100

    moved to J's

---
+++ stage-0/handlers.js
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const handleHelloRequest = safelyRun(async (req, res) => {
+  const clientIp = req.ip;
+  const mockIp = clientIp === "::1" ? "24.48.0.1" : clientIp;
+  const visitorName = req.query.visitor_name;
+
+  if (!visitorName) {
+    return res.status(400).json({ error: "visitor_name is required", status: false });
+  }
+
+  const ipApiResponse = await fetch("http://ip-api.com/json/" + mockIp);
+  const ipApiData = await ipApiResponse.json();
+
+  const weatherApiResponse = await fetch(
+    "https://api.weatherapi.com/v1/current.json?q=" + ipApiData.city + "&key=" + process.env.W_KEY
+  );
+  const weatherApiData = await weatherApiResponse.json();
+
+  if (weatherApiData.error) {
+    return res.status(400).json({ error: weatherApiData.error.message, status: false });
+  }
+
+  const greeting = `Hello, ${visitorName}!, the temperature is ${weatherApiData.current.temp_c} degrees Celsius in ${ipApiData.city}`;
+
+  const location = clientIp === "::1" ? "localhost" : ipApiData.city;
+
+  return res.json({
+    client_ip: clientIp,
+    greeting,
+    location,
+  });
+});
+
+function safelyRun(fn) {
+  return async (req, res) => {
+    try {
+      return await fn(req, res);
+    } catch (e) {
+      console.error(e, e.cause);
+      res.status(500).send("Internal Server Error");
+    }
+  };
+}
+
+export { handleHelloRequest };
---
+++ stage-0/index.js
@@ -0,0 +1,34 @@
+import { handleHelloRequest } from "./handlers.js";
+import { server } from "./server.js";
+import dotenv from "dotenv";
+import http from "http";
+
+dotenv.config();
+
+// Middleware to set client IP
+server.use((req, res, next) => {
+  const ip = req.ip || "0.0.0.0";
+  
+  if (!ip) {
+    req.clientIp = {
+      port: 3000,
+      family: "IPv4",
+      address: "0.0.0.0",
+    };
+  } else {
+    console.log("Client IP:", ip);
+    req.clientIp = ip;
+  }
+  next();
+});
+
+// Define route
+server.get("/api/hello", handleHelloRequest);
+
+// Create and start server
+const server2 = http.createServer(server);
+
+const port = 3000;
+server2.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
